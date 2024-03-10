841b2b4c6a0d9cc76d41c3f3f3c56d86c7df56b3
Androff <rageclock23@gmail.com>
2024-03-10 17:48:55 +0100

    new changes

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+let numberOfFilms;
+
+function detectPersonalLevel() {
+  if (typeof numberOfFilms != "number") {
+    alert("Error");
+  } else if (numberOfFilms < 10) {
+    alert("Malo ti smotrish!");
+  } else if (numberOfFilms < 30) {
+    alert("You are classic haha");
+  } else if (numberOfFilms >= 30) {
+    alert("Congratulations you are movieFan");
+  }
+}
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+  }
+}
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("Последний помотренный фильм"),
+      b = prompt("Оцени этот фильм");
+    if (a.length != 0 && b.length != 0 && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+    } else {
+      alert("Oshibka!");
+      i--;
+    }
+  }
+}
+function showMyDB() {
+  if (!personalMovieDB.privat) {
+    console.log(personalMovieDB);
+  }
+}
+
+function writeYourGeners() {
+  for (let i = 1; i <= 3; i++) {
+    let a = prompt(`Любимый жанр под номером ${i}`);
+    personalMovieDB.geners[i - 1] = a;
+  }
+}
 
 let personalMovieDB = {
   count: numberOfFilms,
@@ -7,11 +49,10 @@ let personalMovieDB = {
   geners: [],
   privat: false,
 };
-let lastSeenFilm = prompt("Last film?");
-let filmRaito = prompt("Rait your last film");
-let a = prompt("Last film?");
-let b = prompt("Rait your last film");
-personalMovieDB.movies[lastSeenFilm] = filmRaito;
-personalMovieDB.movies[a] = b;
+writeYourGeners();
 
-console.log(personalMovieDB);
+start();
+
+rememberMyFilms();
+
+detectPersonalLevel();
